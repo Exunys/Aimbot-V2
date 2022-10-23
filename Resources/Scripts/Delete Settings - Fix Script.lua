7d2fa4ad85945c31fe6adc9cc04b0c80b50d25ae
Exunys <76539058+Exunys@users.noreply.github.com>
2022-10-23 17:16:22 +0200

    Automatic Repair

--- Resources/Scripts/Delete Settings - Fix Script.lua
+++ Resources/Scripts/Delete Settings - Fix Script.lua
@@ -1,5 +1,5 @@
 pcall(function()
-	if isfolder("Exunys Developer") then
-		delfolder("Exunys Developer")
+	if isfolder("Exunys Developer/Aimbot") then
+		delfolder("Exunys Developer/Aimbot")
 	end
 end)
