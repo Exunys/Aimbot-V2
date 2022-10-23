861519280bb0e93eb24b2f75510c994b485d7df2
Exunys <76539058+Exunys@users.noreply.github.com>
2022-10-23 17:15:05 +0200

    Factory Reset Settings Manually

--- Resources/Scripts/Manual Factory Reset Settings.lua
+++ Resources/Scripts/Manual Factory Reset Settings.lua
@@ -1,25 +1,27 @@
 getgenv().Aimbot.Settings = {
-    SendNotifications = true,
-    SaveSettings = true, -- Re-execute upon changing
-    ReloadOnTeleport = true,
-    Enabled = true,
-    TeamCheck = false,
-    AliveCheck = true,
-    WallCheck = false, -- Laggy
-    Sensitivity = 0, -- Animation length (in seconds) before fully locking onto target
-    TriggerKey = "MouseButton2",
-    Toggle = false,
-    LockPart = "Head" -- Body part to lock on
+	SendNotifications = true,
+	SaveSettings = true, -- Re-execute upon changing
+	ReloadOnTeleport = true,
+	Enabled = true,
+	TeamCheck = false,
+	AliveCheck = true,
+	WallCheck = false, -- Laggy
+	Sensitivity = 0, -- Animation length (in seconds) before fully locking onto target
+	ThirdPerson = false, -- Uses mousemoverel instead of CFrame to support locking in third person (could be choppy)
+	ThirdPersonSensitivity = 3, -- Boundary: 0.1 - 5
+	TriggerKey = "MouseButton2",
+	Toggle = false,
+	LockPart = "Head" -- Body part to lock on
 }
 
 getgenv().Aimbot.FOVSettings = {
-    Enabled = true,
-    Visible = true,
-    Amount = 90,
-    Color = "255, 255, 255",
-    LockedColor = "255, 70, 70",
-    Transparency = 0.5,
-    Sides = 60,
-    Thickness = 1,
-    Filled = false
+	Enabled = true,
+	Visible = true,
+	Amount = 90,
+	Color = "255, 255, 255",
+	LockedColor = "255, 70, 70",
+	Transparency = 0.5,
+	Sides = 60,
+	Thickness = 1,
+	Filled = false
 }
